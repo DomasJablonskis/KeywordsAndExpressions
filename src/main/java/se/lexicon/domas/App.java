40fe60782af5dea8c86e4dad4bbe3a41be26d0d1
Domas <domas.jablonskis@gmail.com>
2020-11-24 10:21:41 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/domas/App.java
@@ -0,0 +1,21 @@
+package se.lexicon.domas;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // a mile is equal to 1.609344
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is a expression");
+        }
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+    }
+}
